7f756951d32795e702dc4fe30d30026b9d243109
Ahsan Raza <167077860+Ahsan-Ra2a@users.noreply.github.com>
2024-06-05 23:19:43 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,82 @@
+// functions for semester 1 papers 
+function cs(){
+    window.open("semester.html");
+}
+function semester1(){
+    window.open("Courses.html");
+}
+
+function programming(){
+    window.open("https://drive.google.com/drive/folders/1Of7vZzUyx5h3L6jnrg5FZshynZxnDwaG");
+}
+function ict() {
+        window.open("PDF.html");
+}
+function ict1(){
+    window.open("F13 ITC Mid2.pdf");
+}
+function ict2(){
+    window.open("F13 ITC Mid1.pdf");
+}
+function ict3(){
+    window.open("F14 ITC Mid1.pdf");
+}
+function ict4(){
+    window.open("F14 ITC Mid2.pdf");
+}
+function ict5(){
+    window.open("F15 ITC Mid1.pdf");
+}
+function ict6(){
+    window.open("ict final 1.pdf");
+}
+
+function physics(){
+    window.open("https://drive.google.com/drive/folders/1Ux_aVA88zJm0qKWFxSMdZ1uYWxg-xQ9x");
+}
+function cal(){
+    window.open("https://drive.google.com/drive/folders/1KPp3h2u1jgVkDzp8TOtBAUcnSzPhlOUL");
+}
+function islamiat(){
+    window.open("");
+}
+function english(){
+    window.open("https://drive.google.com/drive/folders/1sNBE5TSVgpQO7MOhC-aCAeviHuGFERMZ");
+}
+function programming_lab(){
+    window.open("https://drive.google.com/drive/folders/1J0dBZ26NSJxB1-jOXz5OXWFEC1cbJYFb");
+}
+function english_lab(){
+    window.open("");
+}
+
+// functions for semester 2 papers
+function oop(){
+    window.open("https://drive.google.com/drive/folders/1_mSJLGTnKUhOnjOAUjOpbHugoDVClENL");
+}
+function dld(){
+    window.open("https://drive.google.com/drive/folders/1nZ3dIgQ6AoVlABqqSolLYF7SOHXkU_lZ");
+}
+function de(){
+    window.open("https://drive.google.com/drive/folders/1NVzp8vJ8xJWdBJfQIdrfw_yHW9fsamni");
+}
+function cps(){
+    window.open("https://drive.google.com/drive/folders/1sxn_xhwMY6-NCfb41JE5WhUMAmm4xH__");
+}
+function opp_lab(){
+    window.open("https://drive.google.com/drive/folders/1LjZJ6_PiUIC62y4R84hWeBgkF9MpjRao");
+}
+// function for sem 3
+function dsa(){
+    window.open("https://drive.google.com/drive/folders/1gTZJ4PpqW1TFsNLH8BrAM2AZ7AnaBnKG");
+}
+function ds(){
+    window.open("https://drive.google.com/drive/folders/1fxJjYrOBk_vKAcyYhaoKomdaDtIeQAuv");
+}
+function la(){
+    window.open("https://drive.google.com/drive/folders/1wUbRC1a_F4gT9uy-0ufaOFfxS9pkdhmn");
+}
+function coal(){
+    window.open("https://drive.google.com/drive/folders/190QbJUsk23WoU0452V2zaKuQo-csvUpw");
+}
+// function for sem 4
